1e73960761979aa42f91d1422c30c807140fe994
mark-kibo <kibochamark@gmail.com>
2023-07-12 19:17:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const writeCards=(array, event)=>{
+    let messages=[];
+    for(let i=0; i < array.length; i++){
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+
+const countDown=(number)=>{
+    let i=number;
+    while(i >= 0){
+        console.log(number);
+        i--;
+        number = number -1;
+    }
+}
+
+
+countDown(10);
